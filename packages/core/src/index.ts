a6cc9862049738ea9732a21c09bd7fbd4adb2dd2
fabiorigam <fabio.rigamonti@icloud.com>
2023-09-07 15:23:05 +0200

    First commit
    First commit

---
+++ packages/core/src/index.ts
@@ -0,0 +1,9 @@
+console.log("Hello from core package")
+
+export const add = (a: number, b: number) => {
+    return a + b
+}
+
+export const subtract = (a: number, b: number) => {
+    return a - b
+}
---
+++ packages/network/src/index.ts
@@ -0,0 +1,4 @@
+import * as core from "core"
+
+console.log("Hello from core network")
+core.add(1, 2);
