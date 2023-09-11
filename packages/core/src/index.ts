881e801850468783d41b7288323f41de231ae838
fabiorigam <fabio.rigamonti@icloud.com>
2023-09-11 11:24:37 +0200

    add example to test build of external libraries

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,9 @@
+import { ethers } from "ethers";
+
 console.log("Hello from core package")
+
+const eth = ethers.parseEther("1.0");
+console.log(eth.toString());
 
 export const add = (a: number, b: number) => {
     return a + b
